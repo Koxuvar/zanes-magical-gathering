1b6a7e2eb9c4c2369ad3711fc63326e70600b90e
Connor Sullivan <connor@mmvdesigns.com>
2021-06-30 00:07:44 -0500

    updated showcase to actually search for cards now

--- app/components/Showcase.js
+++ app/components/Showcase.js
@@ -10,6 +10,7 @@ const Showcase = () => {
 
     const [search, setSearch] = useState([]);
     const [results, setResults] = useState([]);
+    const [card, setCard] = useState([]);
 
     const debouncedSearchTerm = useDebounce(search, 500);
 
@@ -26,7 +27,7 @@ const Showcase = () => {
             {
                 if(res.data.status === 'error')
                 {
-                    throw new Error('res.data.message');
+                    throw new Error(res.data.message);
                 }
 
                 setResults(res.data.data);
@@ -39,13 +40,40 @@ const Showcase = () => {
         setSearch(event.target.value);
     }
 
+    const handleSubmit = (e) =>
+    {
+        if(e.key != 'Enter')
+        {
+            return; 
+        }
+
+        const s = e.target.value;
+
+        if(!s)
+        {
+            return;
+        }
+
+        API.searchCards(s)
+        .then((res) =>
+        {
+            if(res.data.status === 'error')
+            {
+                throw new Error('API CALL ERROR: ' + res.data.message);
+            }
+
+            setCard(res.data);
+        });
+    }
+
     return (
         <>
             <div className={style.container}>
                 <h1>Learning magic made easy</h1>
                 <h2>a card search and deck builder for the Standard format</h2>
 
-                <input className={style.search} type='text' placeholder='search for cards here...' value={search} onChange={handleInputChange}></input>
+                <input className={style.search} type='text' placeholder='search for cards here...' value={search} onChange={handleInputChange} onKeyPress={handleSubmit}></input>
+                
 
             </div>
             <div className={style.text_hold}>
--- app/utils/API.js
+++ app/utils/API.js
@@ -5,5 +5,11 @@ export default
     searchTerms: function(query)
     {
         return axios.get(`https://api.scryfall.com/cards/autocomplete?q=${query}`)
+    },
+
+    searchCards: function(query)
+    {
+        const searchCl = query.replace(/\s/g, '+');
+        return axios.get(`https://api.scryfall.com/cards/named?fuzzy=${searchCl}`);
     }
 }

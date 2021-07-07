03dcc127f21408447f082e5f336c773ca939bdeb
Alexandra Grassl <eai.alexandra@gmail.com>
2021-07-07 08:28:26 -0500

    auth folder created under api directory in pages with dynamic authjs file

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.scss'
+import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+  <UserProvider>
+  <Component {...pageProps} />
+  </UserProvider>
+
+  );
+
 }
 
-export default MyApp
+// export default MyApp

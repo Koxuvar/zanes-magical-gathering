8fd8570280989da1c50ca9184861d282da7a20c8
Alexandra Grassl <eai.alexandra@gmail.com>
2021-07-14 18:48:42 -0500

    cleared out seeds and unneeded folders

---
+++ components/Statslayout.js
@@ -0,0 +1,32 @@
+
+import React from 'react'
+import styles from '../styles/Statslayout.module.scss'
+
+
+const Statslayout = () => {
+    return (
+
+        <div className={styles.container}>
+            <div className={styles.info}>
+                <div className={styles.heading}>
+           <h2> hot prof pic </h2>
+            </div>
+            </div>
+            <div className={styles.username}>
+                Username:
+
+            </div>
+            <div className={styles.games}>
+                Games Played: 
+            </div>
+            <div className={styles.last}>
+                Last Login: 
+            </div>
+
+            </div>
+
+)
+    }
+
+
+export default Statslayout 
---
+++ pages/ProfileStats.js
@@ -0,0 +1,18 @@
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
+import Statslayout from '../components/Statslayout'
+
+
+const ProfileStats = () => {
+    return (
+        <>
+        <Nav />
+        <Statslayout />
+        <Footer />
+
+        </>
+
+    )
+}
+
+export default ProfileStats

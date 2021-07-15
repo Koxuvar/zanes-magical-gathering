9fbaec38c7a7727efaa72890e31e120d81665338
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-07-14 20:51:23 -0500

    yummy yummy hamburger

--- components/Nav.js
+++ components/Nav.js
@@ -4,27 +4,41 @@ import style from '../styles/Nav.module.scss'
 const Nav = () => {
     return (
         <nav className={style.header}>
-            <ul>
-                
-                <li>
-                    <Link href='/'>home</Link>
-                </li>
-                <li>
-                    <Link href='/about'>about</Link>
-                </li>
-                <li>
-                    <Link href='/Profile'>profile</Link>
-                </li>
-                <li>
-                    <Link href='/Play'>play</Link>
-                </li>
-                {/* <li className={style.push}>
-                    <Link href='/SignIn'>sign in</Link>
-                </li>
-                <li>
-                    <Link href='/Register'>register</Link>
-                </li> */}
-            </ul>
+
+            <div className={style.menu_wrap}>
+                <input type="checkbox" className={style.toggler}/>
+                <div className={style.hamburger}><div></div></div>
+                <div className={style.menu}>
+                    <div>
+                        <div>
+                            <ul>
+                        
+                                <li>
+                                    <Link href='/'>home</Link>
+                                </li>
+                                <li>
+                                    <Link href='/about'>about</Link>
+                                </li>
+                                <li>
+                                    <Link href='/Profile'>profile</Link>
+                                </li>
+                                <li>
+                                    <Link href='/Play'>play</Link>
+                                </li>
+                                <li>
+                                    <Link href='/Logs'>game log</Link>
+                                </li>
+                                <li>
+                                    <Link href='/SignIn'>sign in</Link>
+                                </li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            
         </nav>
     )
 }
